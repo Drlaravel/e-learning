abfff3b67041efecb456feed6cf428bf673318a2
Zephinax <Zephinax@gmail.com>
2024-04-04 15:06:09 +0330

    add header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import React from 'react';
+import './Header.css';
+import Logo from './../../assets/img/logo/logo.png';
+import { IoSearch } from 'react-icons/io5';
+import { FaChevronDown } from 'react-icons/fa';
 
 export default function header() {
   return (
-    <div>header</div>
-  )
+    <div className='navContainer'>
+      <div className='navWrapper'>
+        <div className='navSearch'>
+          <div className='navLogo'>
+            <img src={Logo} alt='navLogo' />
+          </div>
+          <div className='navSearchInput'>
+            <IoSearch size={20} />
+            <input
+              type='text'
+              name='navSearch'
+              id='navSearch'
+              placeholder='Want to Learn?'
+            />
+            <div className='dropDown'>
+              <span>Explore</span>
+              <FaChevronDown className='downIco' />
+            </div>
+          </div>
+        </div>
+        <div className='navLinks'>
+          <li className='active'>Home</li>
+          <li>About us</li>
+          <li>Courses</li>
+          <li>Contact us</li>
+          <li>FAQ's</li>
+        </div>
+        <div className='navActions'>
+          <span className='navSign'>Sign in</span>
+          <span className='navCreateAccount'>Create free account</span>
+        </div>
+      </div>
+    </div>
+  );
 }
